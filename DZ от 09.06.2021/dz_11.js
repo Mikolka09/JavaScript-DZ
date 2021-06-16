1941556edb144e7389b0149e0b0f88f1650128e3
Микола Матіос <mikolka09@gmail.com>
2021-06-16 17:33:01 +0300

    16.06.2021

--- DZ от 09.06.2021/dz_11.js
+++ DZ от 09.06.2021/dz_11.js
@@ -25,21 +25,35 @@ window.onload = function () {
         myMap.setCenter(location);
 
         let marker = new google.maps.Marker({
-            position:location,
+            position: location,
             map: myMap,
-            title:'Вы навели на метку!\nНажмите для получения информации!'
+            title: 'Вы навели на метку!\nНажмите для получения информации!'
         });
-    
+
         marker.setMap(myMap);
 
         let infowindow = new google.maps.InfoWindow({
-            content:`<h3 style="text-align: center;">Это ваше местоположение</h3><p>Ваши координаты: Широта: ${position.coords.latitude};  
+            content: `<h2 style="text-align: center;">Это ваше местоположение</h2><p style ="font-weight: 600">Ваши координаты: Широта: ${position.coords.latitude};  
             Долгота: ${position.coords.longitude}</p>`
         });
-        marker.addListener("click", function(){
+        marker.addListener("click", function () {
             infowindow.open(myMap, marker);
         })
-       
+
         res.innerHTML = "Местоположение отмечено на карте.";
     }
+
+    //Данные о браузере
+    document.getElementById("appName").innerHTML =
+        "Имя браузера как приложения - " + navigator.appName;
+    document.getElementById("appCodeName").innerHTML =
+        "Кодовое имя браузера - " + navigator.appCodeName;
+    document.getElementById("product").innerHTML =
+        "Движок браузера - " + navigator.product;
+    document.getElementById("appVersion").innerHTML =
+        "Версия браузера - " + navigator.appVersion;
+    document.getElementById("userAgent").innerHTML =
+        "Пользовательский агент браузера - " + navigator.userAgent;
+    document.getElementById("onLine").innerHTML =
+        "Браузер подключен к сети Интернет? - " + `${(navigator.onLine)? 'ДА!':'НЕТ!'}`;
 }
